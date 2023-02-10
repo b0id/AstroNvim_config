825a79d3f8a5a6bca468640910bfe427d3b0b8fd
b0id <b0id.lonewolf.dev@gmail.com>
2023-02-09 23:03:02 -0500

    update init.lua

--- init.lua
+++ init.lua
@@ -72,11 +72,14 @@ local config = {
 
   -- Set dashboard header
   header = {
-    " █████  ███████ ████████ ██████   ██████",
-    "██   ██ ██         ██    ██   ██ ██    ██",
-    "███████ ███████    ██    ██████  ██    ██",
-    "██   ██      ██    ██    ██   ██ ██    ██",
-    "██   ██ ███████    ██    ██   ██  ██████",
+    "        ██       ████  ██      ██",
+    "       ░██      █░░░██░░      ░██",
+    "       ░██     ░█  █░█ ██     ░██",
+    "       ░██████ ░█ █ ░█░██  ██████",
+    "       ░██░░░██░██  ░█░██ ██░░░██",
+    "       ░██  ░██░█   ░█░██░██  ░██",
+    "       ░██████ ░ ████ ░██░░██████",
+    "        ░░░░░    ░░░░  ░░  ░░░░░░",  
     " ",
     "    ███    ██ ██    ██ ██ ███    ███",
     "    ████   ██ ██    ██ ██ ████  ████",
@@ -218,6 +221,22 @@ local config = {
   -- Configure plugins
   plugins = {
     init = {
+        {
+        "simrat39/rust-tools.nvim",
+        after = { "mason-lspconfig.nvim" },
+        -- Is configured via the server_registration_override installed below!
+        config = function()
+          require("rust-tools").setup {
+            server = astronvim.lsp.server_settings "rust_analyzer",
+            tools = {
+              inlay_hints = {
+                parameter_hints_prefix = "  ",
+                other_hints_prefix = "  ",
+              },
+            },
+          }
+        end,
+      },
       -- You can disable default plugins as follows:
       -- ["goolord/alpha-nvim"] = { disable = true },
 
